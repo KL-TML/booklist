2365a2e8b90009070c78276f0fb5b266135a108a
KYLE LIPTON <lipton.kyle@gmail.com>
2017-12-16 13:35:25 -0500

    added csv render

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,20 @@
 class BooksController < ApplicationController
 
   def index
+    @books = Books.all
+
     respond_to do |format|
-      format.txt
+      format.text {render }
     end
+
+    format.text do
+      render: index
+    end
+
+    format.csv do
+      render plain: Book.generate_csv(book_list)
+    end
+
   end
 
 end
